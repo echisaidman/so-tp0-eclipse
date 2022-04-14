e3f5802b5f2b8fc840404a9dc6b7af6ce69adb4a
Ezequiel Saidman <esaidman@frba.utn.edu.ar>
2022-04-14 12:05:05 -0300

    Agrego .h para client

--- client/src/client.c
+++ client/src/client.c
@@ -1,7 +1,25 @@
+#include "client.h"
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+	sayHelloWorld();
+
+	char cwd[PATH_MAX];
+	if (getcwd(cwd, sizeof(cwd)) != NULL) {
+		printf("Current path: %s\n", cwd);
+	}
+	else {
+		perror("getcwd() error");
+		return 1;
+	}
+
+	return 0;
+}
+
+void sayHelloWorld() {
+	printf("Hello World!\n");
 }
